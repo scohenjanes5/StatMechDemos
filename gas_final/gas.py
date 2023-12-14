910aa917d31ca002fca4c0e54c269ead4bb187df
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2023-12-14 10:44:00 -0500

    Comments

--- gas_final/gas.py
+++ gas_final/gas.py
@@ -17,43 +17,43 @@ def compute_new_v(v1, v2, r1, r2):
     return v1new, v2new
 
 def motion(r, v, ids_pairs, ts, dt, d_cutoff):
-    rs = torch.zeros((ts, r.shape[0], r.shape[1])).to(device)
-    vs = torch.zeros((ts, v.shape[0], v.shape[1])).to(device)
+    rs = torch.zeros((ts, r.shape[0], r.shape[1])).to(device) #Store positions at each time step
+    vs = torch.zeros((ts, v.shape[0], v.shape[1])).to(device) #Store velocities at each time step
     # Initial State
     rs[0] = r
     vs[0] = v
     for i in progress.track(range(1,ts), description='Simulating Steps'):
-        ic = ids_pairs[get_deltad2_pairs(r, ids_pairs) < d_cutoff**2]
-        v[:,ic[:,0]], v[:,ic[:,1]] = compute_new_v(v[:,ic[:,0]], v[:,ic[:,1]], r[:,ic[:,0]], r[:,ic[:,1]])
+        ic = ids_pairs[get_deltad2_pairs(r, ids_pairs) < d_cutoff**2] #indices of colliding particles
+        v[:,ic[:,0]], v[:,ic[:,1]] = compute_new_v(v[:,ic[:,0]], v[:,ic[:,1]], r[:,ic[:,0]], r[:,ic[:,1]]) #update velocities
         
-        v[0,r[0]>1] = -torch.abs(v[0,r[0]>1])
-        v[0,r[0]<0] = torch.abs(v[0,r[0]<0])
-        v[1,r[1]>1] = -torch.abs(v[1,r[1]>1])
-        v[1,r[1]<0] = torch.abs(v[1,r[1]<0])
+        v[0,r[0]>1] = -torch.abs(v[0,r[0]>1]) #particles that hit the right wall bounce back
+        v[0,r[0]<0] = torch.abs(v[0,r[0]<0]) #particles that hit the left wall bounce back
+        v[1,r[1]>1] = -torch.abs(v[1,r[1]>1]) #particles that hit the top wall bounce back
+        v[1,r[1]<0] = torch.abs(v[1,r[1]<0]) #particles that hit the bottom wall bounce back
         
-        r = r + v*dt
-        rs[i] = r
-        vs[i] = r
+        r = r + v*dt #update positions according to velocities
+        rs[i] = r #store positions
+        vs[i] = r #store velocities
     return rs, vs
 
 N = 3000
 dt = 8e-6
 t_steps = 2000
 v0 = 500
-L = 1
-r = torch.rand((2,N)).to(device)
-ixr = r[0]>0.5 
-ixl = r[0]<=0.5 
+L = 1 #box size
+r = L * torch.rand((2,N), device=device) #X,Y coordinates in each row
+ixr = r[0]>0.5 #particles that start on the right
+ixl = r[0]<=0.5 #particles that start on the left
 ids = torch.arange(N)
 ids_pairs = torch.combinations(ids,2).to(device)
-v = torch.zeros((2,N)).to(device)
-v[0][ixr] = -v0
-v[0][ixl] = v0
+v = torch.zeros((2,N)).to(device) #X,Y velocities in each row
+v[0][ixr] = -v0 #particles on the right move left
+v[0][ixl] = v0 #particles on the left move right
 radius = 0.005
 rs, vs = motion(r, v, ids_pairs, ts=t_steps, dt=dt, d_cutoff=2*radius)
 
-plt.scatter(*rs[471].cpu())
 plt.xlim(0,1)
 plt.ylim(0,1)
+plt.scatter(*rs[473].cpu())
 plt.show()
 
