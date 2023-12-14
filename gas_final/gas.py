0768cd74bfcd09688c66e98b9791bda6f142599b
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2023-12-14 10:56:12 -0500

    periodic or reflective box

--- gas_final/gas.py
+++ gas_final/gas.py
@@ -16,7 +16,7 @@ def compute_new_v(v1, v2, r1, r2):
     v2new = v2 - torch.sum((v1-v2)*(r1-r2), axis=0)/torch.sum((r2-r1)**2, axis=0) * (r2-r1)
     return v1new, v2new
 
-def motion(r, v, ids_pairs, ts, dt, d_cutoff):
+def motion(r, v, ids_pairs, ts, dt, d_cutoff, box_size=1, box_type='periodic'):
     rs = torch.zeros((ts, r.shape[0], r.shape[1])).to(device) #Store positions at each time step
     vs = torch.zeros((ts, v.shape[0], v.shape[1])).to(device) #Store velocities at each time step
     # Initial State
@@ -25,11 +25,18 @@ def motion(r, v, ids_pairs, ts, dt, d_cutoff):
     for i in progress.track(range(1,ts), description='Simulating Steps'):
         ic = ids_pairs[get_deltad2_pairs(r, ids_pairs) < d_cutoff**2] #indices of colliding particles
         v[:,ic[:,0]], v[:,ic[:,1]] = compute_new_v(v[:,ic[:,0]], v[:,ic[:,1]], r[:,ic[:,0]], r[:,ic[:,1]]) #update velocities
-        
-        v[0,r[0]>1] = -torch.abs(v[0,r[0]>1]) #particles that hit the right wall bounce back
-        v[0,r[0]<0] = torch.abs(v[0,r[0]<0]) #particles that hit the left wall bounce back
-        v[1,r[1]>1] = -torch.abs(v[1,r[1]>1]) #particles that hit the top wall bounce back
-        v[1,r[1]<0] = torch.abs(v[1,r[1]<0]) #particles that hit the bottom wall bounce back
+
+        if box_type == 'periodic':
+            #impose periodic boundary conditions
+            r[0,r[0]>box_size] = r[0,r[0]>box_size] - box_size #particles that hit the right wall reappear on the left
+            r[0,r[0]<0] = r[0,r[0]<0] + box_size #particles that hit the left wall reappear on the right
+            r[1,r[1]>box_size] = r[1,r[1]>box_size] - box_size #particles that hit the top wall reappear on the bottom
+            r[1,r[1]<0] = r[1,r[1]<0] + box_size #particles that hit the bottom wall reappear on the top
+        elif box_type == 'reflective':
+            v[0,r[0]>box_size] = -torch.abs(v[0,r[0]>box_size]) #particles that hit the right wall bounce back
+            v[0,r[0]<0] = torch.abs(v[0,r[0]<0]) #particles that hit the left wall bounce back
+            v[1,r[1]>box_size] = -torch.abs(v[1,r[1]>box_size]) #particles that hit the top wall bounce back
+            v[1,r[1]<0] = torch.abs(v[1,r[1]<0]) #particles that hit the bottom wall bounce back
         
         r = r + v*dt #update positions according to velocities
         rs[i] = r #store positions
@@ -40,7 +47,7 @@ N = 3000
 dt = 8e-6
 t_steps = 2000
 v0 = 500
-L = 1 #box size
+L = 100 #box size
 r = L * torch.rand((2,N), device=device) #X,Y coordinates in each row
 ixr = r[0]>0.5 #particles that start on the right
 ixl = r[0]<=0.5 #particles that start on the left
@@ -50,9 +57,9 @@ v = torch.zeros((2,N)).to(device) #X,Y velocities in each row
 v[0][ixr] = -v0 #particles on the right move left
 v[0][ixl] = v0 #particles on the left move right
 radius = 0.005
-rs, vs = motion(r, v, ids_pairs, ts=t_steps, dt=dt, d_cutoff=2*radius)
+rs, vs = motion(r, v, ids_pairs, ts=t_steps, dt=dt, d_cutoff=2*radius, box_size=L)
 
-plt.scatter(*rs[473].cpu())
+plt.scatter(*rs[-1].cpu())
 plt.xlim(0,L)
 plt.ylim(0,L)
 plt.show()
